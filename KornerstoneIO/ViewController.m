efdb9ed0d29b9b86f344023bd80799d63e3c287c
DetroitLabs <DetroitLabs@DetroitLabss-MacBook-Pro-2.local>
2016-05-25 14:06:46 -0400

    adding the dropdown to view controller

--- KornerstoneIO/ViewController.m
+++ KornerstoneIO/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <UIKit/UIKit.h>
 
-@interface ViewController ()
+
+@interface ViewController() <UIPickerViewDataSource, UIPickerViewDelegate>
 
 @end
 
@@ -16,12 +18,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self createCategoriesArray];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+- (void)createCategoriesArray{
+    _categories= [[NSArray alloc]initWithObjects:@"Coffee", @"Beer", nil];
+    
+    // Connect data
+    _categoryDropDown.dataSource = self;
+    self.categoryDropDown.delegate = self;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+
+// The number of rows of data
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return _categories.count;
+}
+
+
+// The data to return for the row and component (column) that's being passed in
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return _categories[row];
 }
 
 @end
