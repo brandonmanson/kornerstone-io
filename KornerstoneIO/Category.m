dae25be3e9ad473a238afb7c96a1397230ad22dd
Sarmila <sarmila@Brandons-MacBook-Pro-2.local>
2016-05-25 11:23:44 -0400

    Added Category class

---
+++ KornerstoneIO/Category.m
@@ -0,0 +1,13 @@
+//
+//  Category.m
+//  KornerstoneIO
+//
+//  Created by Sarmila on 5/25/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "Category.h"
+
+@implementation Category
+
+@end
