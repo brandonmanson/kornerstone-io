85c0d2ec59a95277ab291c5cd56e44d2c75cef11
Brandon Manson <brandon.manson@detroitlabs.com>
2016-05-25 10:19:03 -0400

    initial commit with xcode project

---
+++ KornerstoneIO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KornerstoneIO
+//
+//  Created by DetroitLabs on 5/25/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
